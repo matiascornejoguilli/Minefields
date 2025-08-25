ec55cb3853434c6e98721425d5a15c857a5b9d60
JoaquinDAlessandro <joaquin.dalessandro@globant.com>
2025-08-25 14:55:07 -0300

    testing v2

--- tests/uniTestingcpp/board.tests.cpp
+++ tests/uniTestingcpp/board.tests.cpp
@@ -1,19 +1,9 @@
 #include <gtest/gtest.h>
 #include <uniTestingcpp/Board.h>
 #include <uniTestingcpp/Game.h>
+#include <uniTestingcpp/utils.h>
 
-/* TEST(Board, should_change_coord_status)
-{
-    Board board(6,5);
-    setCellStatus(board.grid, 3, 4, Owner::Player); 
-    //board.grid[3][4].owner = Owner::Player; //this returns true
-    EXPECT_EQ(board.grid[3][4].owner, Owner::Player);
-}
-TEST(Board, should_return_nullptr_when_trying_to_access_invalid_coordinate)
-{
-    Board board(4, 4);
 
-} */
 
 namespace boardTests
 {
@@ -24,4 +14,13 @@ TEST(Board, should_change_coord_status)
     //board.grid[3][4].owner = Owner::Player; //this returns true
     EXPECT_EQ(board.grid[3][4].owner, Owner::Player);
 }
+TEST(Board, should_change_board_size)
+{
+    size_t width = 2;
+    size_t heigth = 3;
+    Board board{width, heigth};
+    EXPECT_EQ(board.grid.size(), heigth);
+    EXPECT_EQ(board.grid[0].size(), width);
+}
+
 } // namespace boardTests
--- tests/uniTestingcpp/game.tests.cpp
+++ tests/uniTestingcpp/game.tests.cpp
@@ -23,4 +23,14 @@ TEST(Game, should_destroy_mine_when_trying_to_guess_it)
     shoot(2, 3, board.grid.at(0).size(), board.grid.size(), minesLeft, minesLeft, board.grid);
     EXPECT_EQ(board.grid[2][3].owner, Owner::Destroy);
 }
+TEST(Board, should_return_false_when_trying_to_shoot_destroyed_mines)
+{
+    Board board(4, 5);
+    Player player(5);
+    setCellStatus(board.grid, 2, 3, Owner::Player);
+    int minesLeft = player.playerMines;
+    shoot(2, 3, board.grid.at(0).size(), board.grid.size(), minesLeft, minesLeft, board.grid);
+    EXPECT_FALSE(shoot(2, 3, board.grid.at(0).size(), board.grid.size(), minesLeft, minesLeft, board.grid));
+    
+}
 } // namespace gameTests
